d4b6df27f5ed3440b68c0d2d256bc47ffa10a665
Claudio Moscoso <cmoscosoz@gmail.com>
2021-09-16 15:12:54 +1200

    styling fonts and titles

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -37,7 +37,7 @@ class TransactionList extends StatelessWidget {
                 children: <Widget>[
                   Text(
                     transactions[index].title,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    style: Theme.of(context).textTheme.headline6,
                   ),
                   Text(
                     DateFormat('dd/MM/yyyy').format(transactions[index].date),

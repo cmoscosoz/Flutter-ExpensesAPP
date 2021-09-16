582845be6ec8db215df284256e67d77311e188b5
Claudio Moscoso <cmoscosoz@gmail.com>
2021-09-16 16:56:07 +1200

    adding chart bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,8 +1,7 @@
-import 'package:expensesapp/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransations;
@@ -27,14 +26,20 @@ class Chart extends StatelessWidget {
           }
         }
 
-        print(DateFormat.E().format(weekDay).substring(0));
+        print(DateFormat.E().format(weekDay).substring(0, 1));
         print(totalSum);
         return {
-          'day': DateFormat.E().format(weekDay).substring(0),
+          'day': DateFormat.E().format(weekDay).substring(0, 1),
           'amount': totalSum
         };
       },
     );
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
   }
 
   @override
@@ -45,7 +50,13 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text('${data['day']} : ${data['amount'].toString()}');
+          return ChartBar(
+            data['day'],
+            data['amount'],
+            totalSpending == 0.0
+                ? 0.0
+                : (data['amount'] as double) / totalSpending,
+          );
         }).toList(),
       ),
     );
